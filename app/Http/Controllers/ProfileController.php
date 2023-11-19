b86502fa1dd6a780dbfd5c94bfc378f4c041453d
Md. Shahariar Rahman <99550336+shahariarr@users.noreply.github.com>
2023-11-19 17:20:17 +0600

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 
 
 class ProfileController extends Controller
 {
-    public function index(Request $request){
-      $id= $request->id;
+    public function index($id){
+      
       $name ="Donal Trump";
       $age = "75";
       $data = [
